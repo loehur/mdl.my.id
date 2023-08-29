2da89e0f7e71f0563802a769f0a26157592effd5
loehur <loehur@gmail.com>
2023-08-29 11:55:49 +0700

    ok mdl22

---
+++ db_jeje.php
@@ -0,0 +1,9 @@
+		<?php
+		date_default_timezone_set('Asia/Jakarta');
+		$serverNameLocal = "localhost";
+		$uid = "mdlmyid_luhur";
+		$pwd = "a123654b";
+		$dbname = "mdlmyid_JEJE";
+
+		$conn = mysqli_connect($serverNameLocal, $uid, $pwd, $dbname);
+		?>
