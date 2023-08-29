2a54e30735317b6b5a06fb3469698d6e2a8460ba
loehur <47594852+loehur@users.noreply.github.com>
2023-08-29 11:59:30 +0700

    Delete db_jeje.php

--- db_jeje.php
+++
@@ -1,9 +0,0 @@
-		<?php
-		date_default_timezone_set('Asia/Jakarta');
-		$serverNameLocal = "localhost";
-		$uid = "mdlmyid_luhur";
-		$pwd = "a123654b";
-		$dbname = "mdlmyid_JEJE";
-
-		$conn = mysqli_connect($serverNameLocal, $uid, $pwd, $dbname);
-		?>
